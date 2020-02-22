b36282aacd6bafc3a66bd94b84cdcf640c2d0727
Olga Latun <olha.latun@gmail.com>
2020-02-22 09:21:51 +0200

    math with JS

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,12 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+const a = 13;
+const b = 12;
+let sum = a + b;
+
+console.log(sum);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
