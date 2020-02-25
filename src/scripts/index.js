4e1e35ae69dc33589e641a2151f5c753bcf72486
Olga Latun <olha.latun@gmail.com>
2020-02-25 21:46:54 +0200

    added arrey

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
-import '../styles/index.scss';
+import "../styles/index.scss";
 
-console.log('webpack starterkit');
+console.log("webpack starterkit");
 
 const a = 13;
 const b = 12;
@@ -11,17 +11,30 @@ console.log(a - b);
 console.log(a * b);
 console.log(a / b);
 
+const fruits = {
+  fruits: "grapes, bananas, oranges"
+};
+
+const sweets = {
+  sweets: "chocolate, peanut butter, ice-cream"
+};
+
+const favouriteFood = [fruits, sweets];
+
 const user = {
-    name: "Olha",
-    lastName: "Latun",
-    age: 26,
-    dateOfBirth: "17.01.1994",
-    heigth: 155,
-    petOwner: true,
-    favouriteFood: "peanut butter, fruits, coffee",
-    getUserNameAndDateOfBirth: function () {
-        console.log("Users' name and birthday is ", this.name + this.lastName + this.dateOfBirth)
-    }
+  name: "Olha",
+  lastName: "Latun",
+  age: 26,
+  dateOfBirth: "17.01.1994",
+  heigth: 155,
+  petOwner: true,
+  getUserNameAndDateOfBirth: function() {
+    console.log(
+      "Users' name and birthday is ",
+      this.name + " " + this.lastName + " " + this.dateOfBirth
+    );
+  }
 };
 console.log(user);
 user.getUserNameAndDateOfBirth();
+console.log(favouriteFood);
