a1817badc7c9f92c84927237650bac027ee188e8
Olga Latun <olha.latun@gmail.com>
2020-03-01 22:25:04 +0200

    bulb project start

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -23,7 +23,12 @@ module.exports = {
       { from: Path.resolve(__dirname, "../public"), to: "public" }
     ]),
     new HtmlWebpackPlugin({
+      filename: "index.html",
       template: Path.resolve(__dirname, "../src/index.html")
+    }),
+    new HtmlWebpackPlugin({
+      filename: "Bulb-project.html",
+      template: Path.resolve(__dirname, "../src/Bulb-project.html")
     })
   ],
   resolve: {
